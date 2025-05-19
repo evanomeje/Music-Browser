5adc053f82a36693ddde3cd583f024749d52ad9e
evan <evanomeje@gmail.com>
2025-05-18 21:43:04 -0600

    final project

---
+++ script.js
@@ -0,0 +1,494 @@
+const data = [
+  {
+    name: "Emergency on Planet Earth",
+    cover: "https://t2.genius.com/unsafe/591x0/https%3A%2F%2Fimages.genius.com%2Ff3a2ae7f7a067000f790e9e077b6aaa1.1000x1000x1.jpg",
+    songs: [
+      ["When You Gonna Learn", "", "3:50"],
+      ["Too Young to Die", "", "6:05"],
+      ["Hooked Up", "", "4:30"],
+      ["If I Like It, I Do It", "", "5:03"],
+      ["Music of the Mind", "", "6:20"],
+      ["Emergency on Planet Earth", "", "4:05"],
+      ["Whatever It Is, I Just Can't Stop", "", "4:05"],
+      ["Blow Your Mind", "", "8:30"],
+      ["Revolution 1993", "", "5:00"],
+      ["Didgin' Out", "", "2:35"]
+    ]
+  },
+  {
+    name: "The Return of the Space Cowboy",
+    cover: "https://t2.genius.com/unsafe/591x0/https%3A%2F%2Fimages.genius.com%2F2fb148fb46f4945e3111b554994cfd4a.1000x1000x1.jpg",
+    songs: [
+      ["Just Another Story", "", "8:05"],
+      ["Stillness in Time", "", "4:15"],
+      ["Half the Man", "", "4:45"],
+      ["Light Years", "", "5:53"],
+      ["Manifest Destiny", "", "6:09"],
+      ["The Kids", "", "5:08"],
+      ["Mr. Moon", "", "5:40"],
+      ["Scam", "", "7:00"],
+      ["Journey to Arnhemland", "", "5:20"],
+      ["Morning Glory", "", "6:20"],
+      ["Space Cowboy", "", "6:25"]
+    ]
+  },
+  {
+    name: "Virtual Insanity ",
+    cover: "https://t2.genius.com/unsafe/591x0/https%3A%2F%2Fimages.genius.com%2F15bc0e186227eb5a0be6a05874a0f549.597x597x1.jpg",
+    songs: [
+    ["Virtual Insanity", "", "5:40"],
+    ]
+  },
+  {
+    name: "Travelling Without Moving",
+    cover: "https://t2.genius.com/unsafe/591x0/https%3A%2F%2Fimages.genius.com%2F71cfb1038754ea00b43f87c2cc800c8a.640x640x1.jpg",
+    songs: [
+      ["Virtual Insanity", "", "5:40"],
+      ["Cosmic Girl", "", "4:05"],
+      ["Use the Force", "", "4:00"],
+      ["Everyday", "", "4:30"],
+      ["Alright", "", "4:25"],
+      ["High Times", "", "6:00"],
+      ["Drifting Along", "", "4:05"],
+      ["Didjerama", "", "2:20"],
+      ["Didjital Vibrations", "", "5:35"],
+      ["Travelling Without Moving", "", "3:40"],
+      ["You Are My Love", "", "4:00"],
+      ["Spend a Lifetime", "", "4:05"],
+      ["Do You Know Where You're Coming From", "", "5:00"]
+    ]
+  },
+  {
+    name: "Synkronized",
+    cover: "https://t2.genius.com/unsafe/591x0/https%3A%2F%2Fimages.genius.com%2F29c251abfa4f5f4d39304f77bc49a34c.600x600x1.jpg",
+    songs: [
+      ["Canned Heat", "", "5:30"],
+      ["Planet Home", "", "4:45"],
+      ["Black Capricorn Day", "", "5:40"],
+      ["Soul Education", "", "4:30"],
+      ["Falling", "", "3:50"],
+      ["Destitute Illusions", "", "5:35"],
+      ["Supersonic", "", "5:15"],
+      ["Butterfly", "", "5:00"],
+      ["Where Do We Go from Here?", "", "4:50"],
+      ["King for a Day", "", "4:50"],
+      ["Deeper Underground", "", "4:45"]
+    ]
+  },
+  {
+    name: "A Funk Odyssey",
+    cover: "https://t2.genius.com/unsafe/591x0/https%3A%2F%2Fimages.genius.com%2F1c289da09a315a2ef20874895ea422a9.640x640x1.jpg",
+    songs: [
+      ["Feels Just Like It Should", "", "4:30"],
+      ["Little L", "", "4:55"],
+      ["You Give Me Something", "", "3:20"],
+      ["Corner of the Earth", "", "5:40"],
+      ["Love Foolosophy", "", "3:45"],
+      ["Stop Don't Panic", "", "4:35"],
+      ["Black Crow", "", "4:05"],
+      ["Main Vein", "", "5:05"],
+      ["Twenty Zero One", "", "5:15"],
+      ["Picture of My Life", "", "6:20"]
+    ]
+  },
+  {
+    name: "Dynamite",
+    cover: "https://t2.genius.com/unsafe/591x0/https%3A%2F%2Fimages.genius.com%2Fc2e2f618ea527f5b461a0cc82bc117c0.1000x1000x1.jpg",
+    songs: [
+      ["Feels Just Like It Should", "", "4:30"],
+      ["Dynamite", "", "3:45"],
+      ["Seven Days in Sunny June", "", "4:00"],
+      ["Electric Mistress", "", "4:15"],
+      ["Starchild", "", "4:50"],
+      ["Love Blind", "", "4:05"],
+      ["Talisman", "", "4:20"],
+      ["Time Won't Wait", "", "4:25"],
+      ["World That He Wants", "", "4:45"],
+      ["Black Devil Car", "", "4:45"],
+      ["Hot Tequila Brown", "", "4:05"],
+      ["(Don't) Give Hate a Chance", "", "4:05"]
+    ]
+  },
+  {
+    name: "High Times (Singles 1992–2006)",
+    cover: "https://t2.genius.com/unsafe/591x0/https%3A%2F%2Fimages.genius.com%2F2d828c0c76c979c0c83ce0beb842282c.500x500x1.jpg",
+    songs: [
+    ["When You Gonna Learn", "", "3:49"],
+    ["Too Young To Die", "", "3:23"],
+    ["Blow Your Mind", "", "3:56"],
+    ["Emergency On Planet Earth", "", "3:37"],
+    ["Space Cowboy", "", "3:37"],
+    ["Virtual Insanity", "", "3:49"],
+    ["Cosmic Girl", "", "3:47"],
+    ["Alright", "", "3:42"],
+    ["High Times", "", "4:10"],
+    ["Deeper Underground", "", "4:46"],
+    ["Canned Heat", "", "3:48"],
+    ["Little L", "", "3:59"],
+    ["Love Foolosophy", "", "3:47"],
+    ["Corner Of The Earth", "", "3:56"],
+    ["You Give Me Something", "", "3:24"],
+    ["Feels Just Like It Should", "", "4:33"],
+    ["Seven Days In Sunny June", "", "4:02"],
+    ["(Don't) Give Hate A Chance", "", "3:50"],
+    ["Runaway", "", "3:46"],
+    ["Radio", "", "4:12"]
+  ]
+  },
+  {
+    name: "Rock Dust Light Star",
+    cover: "https://t2.genius.com/unsafe/591x0/https%3A%2F%2Fimages.genius.com%2F9516101ff5d58160e1c744e9c413adec.1000x1000x1.jpg",
+    songs: [
+      ["Rock Dust Light Star", "", "4:22"],
+      ["White Knuckle Ride", "", "3:35"],
+      ["Smoke and Mirrors", "", "4:36"],
+      ["All Good in the Hood", "", "3:34"],
+      ["Hurtin'", "", "4:02"],
+      ["Blue Skies", "", "4:06"],
+      ["Lifeline", "", "4:40"],
+      ["She's a Fast Persuader", "", "3:42"],
+      ["Two Completely Different Things", "", "4:29"],
+      ["Goodbye to My Dancer", "", "4:08"],
+      ["Never Gonna Be Another", "", "4:50"],
+      ["Hey Floyd", "", "4:10"]
+    ]
+  },
+  {
+    name: "Automaton",
+    cover: "https://t2.genius.com/unsafe/591x0/https%3A%2F%2Fimages.genius.com%2F56306930051b79b7aaf238ae07c6020e.1000x1000x1.jpg",
+    songs: [
+      ["Shake It On", "", "4:11"],
+      ["Automaton", "", "4:47"],
+      ["Cloud 9", "", "3:56"],
+      ["Superfresh", "", "3:48"],
+      ["Hot Property", "", "4:33"],
+      ["Something About You", "", "4:00"],
+      ["Summer Girl", "", "4:45"],
+      ["Nights Out in the Jungle", "", "4:21"],
+      ["Dr Buzz", "", "6:01"],
+      ["We Can Do It", "", "4:06"],
+      ["Vitamin", "", "4:26"],
+      ["Carla", "", "5:47"]
+    ]
+  }
+];
+
+const imageContainer = document.querySelector(".image-container");
+const imageContainerHeight = imageContainer.getBoundingClientRect().height;
+const albumsContainer = document.querySelector(".albums-container");
+const albumsWrapper = document.querySelector(".albums-wrapper");
+const albumTops = [];
+
+// Create album elements (keep your existing album creation code)
+data.forEach((item, i) => {
+  const div = document.createElement("div");
+  const cover = document.createElement("img");
+  cover.classList.add("cover");
+  cover.src = item.cover;
+  cover.alt = item.name;
+  div.appendChild(cover);
+  imageContainer.appendChild(div);
+
+  const album = document.createElement("div");
+  album.classList.add("album");
+  const albumTitle = document.createElement("h2");
+  albumTitle.innerHTML = item.name;
+  album.appendChild(albumTitle);
+  item.songs.forEach((track, n) => {
+    const song = document.createElement("div");
+    song.classList.add("song");
+    const songName = document.createElement("p");
+    songName.innerHTML = `<span>${n + 1}</span>${track[0]}&ensp;<span>${
+      track[1]
+    }</span>`;
+    song.appendChild(songName);
+    const songTime = document.createElement("p");
+    songTime.innerHTML = track[2];
+    song.appendChild(songTime);
+    album.appendChild(song);
+  });
+  albumsWrapper.appendChild(album);
+  albumTops.push(
+    album.getBoundingClientRect().top -
+      imageContainer.getBoundingClientRect().top
+  );
+});
+
+// Curved scrollbar implementation
+const bar = document.querySelector('.scroller__bar');
+const track = document.querySelector('.bar__track');
+const thumb = document.querySelector('.bar__thumb');
+const styles = document.createElement('style');
+document.head.appendChild(styles);
+
+const config = {
+  radius: 16,
+  scrollPadding: 60,
+  stroke: 4,
+  inset: 6,
+  trail: 20,
+  thumb: 70,
+  finish: 5,
+  alpha: 0.8,
+  track: 0.1,
+  color: '#ffffff',
+  cornerLength: 0,
+  offsetCorner: -20,
+  offsetEnd: 10
+};
+
+function syncScrollbar() {
+  const mid = config.radius;
+  const innerRad = Math.max(0, config.radius - (config.inset + config.stroke * 0.5));
+  const padTop = config.inset + config.stroke * 0.5;
+  const padLeft = config.radius * 2 - padTop;
+  
+  bar.setAttribute('viewBox', `0 0 ${config.radius * 2} ${albumsContainer.offsetHeight}`);
+  
+  let d = `
+    M${mid - config.trail},${padTop}
+    ${innerRad === 0 ? '' : `L${mid},${padTop}`}
+    ${
+      innerRad === 0
+        ? `L${padLeft},${padTop}`
+        : `a${innerRad},${innerRad} 0 0 1 ${innerRad} ${innerRad}`
+    }`;
+  thumb.setAttribute('d', d);
+  const cornerLength = Math.ceil(thumb.getTotalLength());
+  config.cornerLength = cornerLength;
+  
+  d = `
+    M${mid - config.trail},${padTop}
+    ${innerRad === 0 ? '' : `L${mid},${padTop}`}
+    ${
+      innerRad === 0
+        ? `L${padLeft},${padTop}`
+        : `a${innerRad},${innerRad} 0 0 1 ${innerRad} ${innerRad}`
+    }
+    L${padLeft},${albumsContainer.offsetHeight - (config.inset + config.stroke * 0.5 + innerRad)}
+    ${
+      innerRad === 0
+        ? `L${padLeft},${albumsContainer.offsetHeight - (config.inset + config.stroke * 0.5)}`
+        : `a${innerRad},${innerRad} 0 0 1 ${-innerRad} ${innerRad}`
+    }
+    L${mid - config.trail},${albumsContainer.offsetHeight - (config.inset + config.stroke * 0.5)}
+  `;
+  
+  thumb.setAttribute('d', d);
+  track.setAttribute('d', d);
+  
+  const trackLength = Math.ceil(track.getTotalLength());
+  document.documentElement.style.setProperty('--track-length', trackLength);
+  document.documentElement.style.setProperty('--track-start', cornerLength);
+}
+
+// Initialize scrollbar
+syncScrollbar();
+
+// Update scrollbar on resize
+window.addEventListener('resize', syncScrollbar);
+
+albumsContainer.addEventListener('scroll', function() {
+  const scrollPercentage = this.scrollTop / (this.scrollHeight - this.clientHeight);
+  const trackLength = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--track-length'));
+  
+  // Set the thumb size based on scroll position (smaller at top, larger at bottom)
+  const minThumbSize = 40;
+  const maxThumbSize = 120;
+  const thumbSize = minThumbSize + (maxThumbSize - minThumbSize) * scrollPercentage;
+  
+  // Calculate the dashoffset based on the scroll percentage
+  const dashOffset = -1 * (trackLength - thumbSize) * scrollPercentage;
+  
+  thumb.style.strokeDasharray = `${thumbSize} ${trackLength}`;
+  thumb.style.strokeDashoffset = dashOffset;
+});
+
+// Keep your existing animation and interaction code
+const maxWidth = 25;
+const imageHeight = 200;
+let activeIndex = 0;
+const images = document.querySelectorAll(".image-container > div");
+let totalWidth = window.innerWidth;
+let isHovering = false;
+const albums = document.querySelectorAll(".album");
+
+function animateImages(input) {
+  let remainingWidth = totalWidth;
+  const widths = generateNumbers(input).finalWeights;
+  const maxWeightIndex = generateNumbers(input).maxWeightIndex;
+  activeIndex = maxWeightIndex;
+
+  albumsContainer.scrollTo({
+    top: albumTops[activeIndex],
+    left: 0,
+    behavior: "smooth",
+  });
+  for (let i = 0; i < data.length; i++) {
+    const image = images[i];
+    const height = widths[i];
+
+    image.style.opacity = "1";
+    image.style.height = `${
+      ((imageContainerHeight / (data.length - 1)) * height) / 10
+    }px`;
+    image.style.filter = "saturate(1)";
+
+    remainingWidth -= height;
+  }
+}
+
+imageContainer.addEventListener("mouseenter", () => {
+  isHovering = true;
+  albumsContainer.style.width = "80%";
+  for (let i = 0; i < data.length; i++) {
+    const image = images[i];
+    image.style.transition = "height 0.1s ease";
+    setTimeout(function() {
+      image.style.transition = "0s";
+    }, 100);
+  }
+});
+
+let lastMouseY = 0;
+let speed = 0;
+let lastMouseMoveTime = Date.now();
+
+let mouseMoveTimeout;
+
+imageContainer.addEventListener("mousemove", (event) => {
+  if (!isHovering) return;
+
+  const currentTime = Date.now();
+  const timeDiff = currentTime - lastMouseMoveTime;
+  speed = (event.clientY - lastMouseY) / timeDiff;
+
+  lastMouseY = event.clientY;
+  lastMouseMoveTime = currentTime;
+
+  clearTimeout(mouseMoveTimeout);
+  mouseMoveTimeout = setTimeout(() => {
+    speed = 0;
+  }, 100);
+
+  const cursorPercentage =
+    (event.clientY - imageContainer.getBoundingClientRect().top) /
+    imageContainerHeight;
+
+  animateImages(easeTiles(cursorPercentage));
+});
+
+let previousActiveIndex = 0;
+let isTimerRunning = false;
+
+function animateBlurGap() {
+  let timer;
+  let transform = `scaleY(${(Math.abs(speed) + 1) * 1}) rotateX(${
+    (speed > 0.5 ? 0.5 : speed < -0.5 ? -0.5 : speed) * 180
+  }deg)`;
+  if (activeIndex !== previousActiveIndex) {
+    isTimerRunning = false;
+    albumsContainer.style.transform = transform;
+    albumsContainer.style.filter = `blur(${Math.abs(speed) * 10}px)`;
+    previousActiveIndex = activeIndex;
+  } else if (!isTimerRunning) {
+    albumsContainer.style.transform = transform;
+    albumsContainer.style.filter = `blur(${Math.abs(speed) * 10}px)`;
+    setTimeout(function () {
+      imageContainer.style.filter = "";
+      albumsContainer.style.filter = "";
+      albumsContainer.style.transform = `scaleY(1) rotateX(0deg)`;
+      isTimerRunning = true;
+    }, 600);
+  }
+
+  requestAnimationFrame(animateBlurGap);
+}
+
+requestAnimationFrame(animateBlurGap);
+
+imageContainer.addEventListener("mouseleave", () => {
+  isHovering = false;
+  albumsContainer.style.width = "90%";
+  for (let i = 0; i < data.length; i++) {
+    const image = images[i];
+    image.style.transition = "height 0.1s ease";
+    setTimeout(function() {
+      image.style.transition = "0s";
+    }, 100);
+  }
+
+  equalizeSizes();
+  for (let i = 0; i < images.length; i++) {
+    images[i].style.opacity = "0.3";
+    images[i].style.filter = "saturate(0)";
+    albums[i].style.gap = `0px`;
+  }
+  images[activeIndex].style.opacity = "1";
+  images[activeIndex].style.filter = "saturate(1)";
+});
+
+function equalizeSizes() {
+  for (let i = 0; i < data.length; i++) {
+    const image = images[i];
+    const height = 100 / images.length;
+
+    image.style.height = `${imageContainerHeight / data.length}px`;
+  }
+}
+
+function generateNumbers(percentage, maxWeight = maxWidth) {
+  const totalSum = 100;
+  const numberOfElements = data.length;
+  const midPoint = percentage * (numberOfElements - 1);
+
+  const rawWeights = Array.from({ length: numberOfElements }, (_, i) => {
+    const distance = Math.abs(i - midPoint);
+    return 1 / (distance + 1);
+  });
+
+  const rawWeightSum = rawWeights.reduce((sum, weight) => sum + weight, 0);
+  const normalizedWeights = rawWeights.map((weight) => weight / rawWeightSum);
+
+  const scaledWeights = normalizedWeights.map((weight) =>
+    Math.min(weight * totalSum, maxWeight)
+  );
+  const scaledWeightSum = scaledWeights.reduce(
+    (sum, weight) => sum + weight,
+    0
+  );
+
+  const redistributionFactor = (totalSum - scaledWeightSum) / scaledWeightSum;
+  const finalWeights = scaledWeights.map(
+    (weight) => weight + weight * redistributionFactor
+  );
+
+  const maxWeightIndex = scaledWeights.indexOf(Math.max(...scaledWeights));
+
+  return { finalWeights, maxWeightIndex };
+}
+
+function easeTiles(t) {
+  t /= 0.5;
+  if (t < 1) return 0.5 * Math.pow(t, 3);
+  t -= 2;
+  return 0.5 * (Math.pow(t, 3) + 2);
+}
+
+equalizeSizes();
+
+albumsContainer.addEventListener("scroll", () => {
+  if (isHovering) return;
+  const scrollPosition = albumsContainer.scrollTop;
+  const activeIndex = albumTops.findIndex(
+    (top) => top > scrollPosition - 500
+  );
+  if (activeIndex === -1) return;
+  for (let i = 0; i < images.length; i++) {
+    images[i].style.opacity = "0.3";
+    images[i].style.filter = "saturate(0)";
+    albums[i].style.gap = `0px`;
+  }
+  images[activeIndex].style.opacity = "1";
+  images[activeIndex].style.filter = "saturate(1)";
+});
